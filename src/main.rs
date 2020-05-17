be44215b01b79a2ff0aee1296660ea2724d5463f
sdttttt <760159537@qq.com>
2020-05-17 17:24:07 +0800

    complete Direction const variable.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
 mod game_object;
 mod timer;
 
-use std::time::{Instant}; // timer
-use game_object::refresh;
 use timer::Timer;
 
 fn main() {
-    let start = Instant::now();
-
     let game_timer = Timer::new(1000);
-    
     game_timer.execute();
-    
-    println!("time cost: {:?} ms", start.elapsed().as_millis());// ms
-    println!("time cost: {:?} us", start.elapsed().as_micros());// us
-    println!("time cost: {:?} ns", start.elapsed().as_nanos());// us
 }
